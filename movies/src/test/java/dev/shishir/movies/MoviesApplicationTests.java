138355dc7f0be068af9294ed5b1edc5f872d9396
Shishir Singh <singhshishir2525@gmail.com>
2026-06-15 23:32:49 +0530

    Initial commit

---
+++ movies/src/test/java/dev/shishir/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shishir.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
